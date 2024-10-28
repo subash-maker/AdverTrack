19ae5156870ddb24e298f294e57f79298556d1d1
subash-maker <65080731+subash-maker@users.noreply.github.com>
2024-10-28 14:33:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+//export default defineConfig({
   plugins: [react()],
-});
+});/
 export default {
   build: {
     rollupOptions: {
